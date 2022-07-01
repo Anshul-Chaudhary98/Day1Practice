30404003c9f9687972fb093695b36d59475f4c31
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 22:59:02 +0530

    Add files via upload

---
+++ MessageDisplay.java
@@ -0,0 +1,10 @@
+package Day1Assignments;
+
+public class MessageDisplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My Day 1 Assignment");
+	}
+
+}
