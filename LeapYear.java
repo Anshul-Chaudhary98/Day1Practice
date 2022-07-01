e52162e2afb5ed42c33541de1f6a2c3c1c5cc19e
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 23:04:05 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+package Day1Assignments;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 0;
+		System.out.print("Enter the Year: ");
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+			if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+				System.out.println("Is a Leap Year!");		
+			}
+			else {
+				System.out.println("Is not a Leap Year!");
+			}
+	}
+
+}
