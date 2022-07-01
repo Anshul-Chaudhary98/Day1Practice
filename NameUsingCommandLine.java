c461439be0e07adf45b40489fec2c1550a77e719
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 23:02:48 +0530

    Add files via upload

---
+++ NameUsingCommandLine.java
@@ -0,0 +1,13 @@
+package Day1Assignments;
+
+public class NameUsingCommandLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
