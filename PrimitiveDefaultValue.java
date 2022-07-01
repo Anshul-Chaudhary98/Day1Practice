baffe07def9713dd144621e0ccb2cc23de59eec5
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 22:59:37 +0530

    Add files via upload

---
+++ PrimitiveDefaultValue.java
@@ -0,0 +1,27 @@
+package Day1Assignments;
+
+public class PrimitiveDefaultValue {
+
+	static byte b;
+	static short s;
+	static int i;
+	static float f;
+	static double d;
+	static long l;
+	static char c;
+	static boolean bl;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Default value of byte: " + b);
+		System.out.println("Default value of short: " + s);
+		System.out.println("Default value of int: " + i);
+		System.out.println("Default value of float: " + f);
+		System.out.println("Default value of double: " + d);
+		System.out.println("Default value of long: " + l);
+		System.out.println("Default value of char: " + c);
+		System.out.println("Default value of boolean: " + bl);
+
+	}
+
+}
