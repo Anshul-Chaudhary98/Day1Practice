df310c0aa933970b341844f50b20ae9a4e3b019c
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 23:00:13 +0530

    Add files via upload

---
+++ TwoStringEqual.java
@@ -0,0 +1,14 @@
+package Day1Assignments;
+
+public class TwoStringEqual {
+
+	static String str1 ="Hello";
+	static String str2 ="Hello World";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str3 = (str1 == str2)?"They are equal": "They are not equal";
+		System.out.println(str3);
+	}
+
+}
