899f0c20dc1097e7d95d02f97f8d216df7a97158
Anshul-Chaudhary98 <101334052+Anshul-Chaudhary98@users.noreply.github.com>
2022-07-01 23:03:37 +0530

    Add files via upload

---
+++ MethodsBlocksStaticvariable.java
@@ -0,0 +1,23 @@
+package Day1Assignments;
+
+public class MethodsBlocksStaticvariable {
+
+	static int a = 25;
+	static int b;
+	static int c;
+	
+	static void newfunction(int c) {
+		System.out.println("a: " + a) ;
+		System.out.println("b: " + b) ;
+		System.out.println("c: " + c) ;
+
+	}
+	static {
+		b = a * 4;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		newfunction(10);
+	}
+
+}
